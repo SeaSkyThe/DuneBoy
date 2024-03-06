11950ceaffa276524905f70b3d00e212d3068ab0
SeaSkyThe <marceloer2011@gmail.com>
2024-03-06 15:20:26 -0300

    feat: implementing write of words to 16bit registers

--- lib/cpu/registers.ml
+++ lib/cpu/registers.ml
@@ -17,6 +17,7 @@ type rr =
   | BC
   | DE
   | HL
+
 type t =
   { mutable a : uint8
   ; mutable b : uint8
@@ -57,9 +58,46 @@ let write_r t reg value =
 ;;
 
 (*https://en.wikipedia.org//wiki/Bitwise_operation*)
-let read_rr t rr = 
-    match rr with
-    | AF -> Uint16.logor (Uint16.shift_left (Uint8.to_uint16 t.a) 8) (Uint8.to_uint16 t.f)
-    | BC -> Uint16.logor (Uint16.shift_left (Uint8.to_uint16 t.b) 8) (Uint8.to_uint16 t.c)
-    | DE -> Uint16.logor (Uint16.shift_left (Uint8.to_uint16 t.d) 8) (Uint8.to_uint16 t.e)
-    | HL -> Uint16.logor (Uint16.shift_left (Uint8.to_uint16 t.h) 8) (Uint8.to_uint16 t.l)
+let read_rr t rr =
+  match rr with
+  | AF ->
+    Uint16.logor
+      (Uint16.shift_left (Uint8.to_uint16 t.a) 8)
+      (Uint8.to_uint16 t.f)
+  | BC ->
+    Uint16.logor
+      (Uint16.shift_left (Uint8.to_uint16 t.b) 8)
+      (Uint8.to_uint16 t.c)
+  | DE ->
+    Uint16.logor
+      (Uint16.shift_left (Uint8.to_uint16 t.d) 8)
+      (Uint8.to_uint16 t.e)
+  | HL ->
+    Uint16.logor
+      (Uint16.shift_left (Uint8.to_uint16 t.h) 8)
+      (Uint8.to_uint16 t.l)
+;;
+
+let write_rr t rr value =
+  match rr with
+  | AF ->
+    { t with
+      a = Uint8.of_int (Uint16.to_int value land 0xFF)
+    ; f = Uint8.of_int (Uint16.to_int (Uint16.shift_right value 8) land 0xFF)
+    }
+  | BC ->
+    { t with
+      b = Uint8.of_int (Uint16.to_int value land 0xFF)
+    ; c = Uint8.of_int (Uint16.to_int (Uint16.shift_right value 8) land 0xFF)
+    }
+  | DE ->
+    { t with
+      d = Uint8.of_int (Uint16.to_int value land 0xFF)
+    ; e = Uint8.of_int (Uint16.to_int (Uint16.shift_right value 8) land 0xFF)
+    }
+  | HL ->
+    { t with
+      a = Uint8.of_int (Uint16.to_int value land 0xFF)
+    ; f = Uint8.of_int (Uint16.to_int (Uint16.shift_right value 8) land 0xFF)
+    }
+;;
